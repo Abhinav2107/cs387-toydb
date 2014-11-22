5f6b37a512db9a913ba221419086acf2f5989332
Abhinav <abhinavgupta21072007@gmail.com>
2014-11-22 20:29:16 +0530

    Write on the mirrored disk as well.

--- pflayer/raid.c
+++ pflayer/raid.c
@@ -22,25 +22,6 @@ void initRAID() {
     }
 }
 
-static RAID_buf_ele* choose(int disk, int fd) {
-    RAID_buf_ele *b = RAID_buf;
-    RAID_buf_ele *b_first = NULL;
-    while(1) {
-        b = b->next;
-        if(b == NULL)
-            return b_first;
-        if(b->disk == -1 && b->pagenum % 2 == disk % 2) {
-            if(b_first == NULL)
-                b_first = b;
-            if(fd == -1)
-                return b_first;
-            if(b->fd == fd)
-                return b;
-        }
-    }
-
-}
-
 static void finish(RAID_buf_ele* b) {
     //printf("Finishing %p\n", b);
     b->prev->next = b->next;
@@ -49,6 +30,41 @@ static void finish(RAID_buf_ele* b) {
     if(b->type == RAID_READ)
         donePFRAID_buf(b->ptr);
     free(b);
+}
+
+static RAID_buf_ele* choose(int disk, int fd) {
+    RAID_buf_ele *b = RAID_buf;
+    RAID_buf_ele *b_first = NULL;
+    while(1) {
+        b = b->next;
+        if(b == NULL)
+            return b_first;
+        if(b->disk == -1 && b->pagenum % 2 == disk % 2 && b->type == RAID_READ) {
+            if(b_first == NULL)
+                b_first = b;
+            if(fd == -1)
+                return b_first;
+            if(b->fd == fd)
+                return b;
+        }
+        if((disk == 0 || disk == 1) && b->pagenum % 2 == disk % 2 && b->type == RAID_WRITE && diskstatus[disk+2] == 0) {
+            if(b_first == NULL) {
+                b_first = b;
+                b = b->next;
+                if(b->fd == diskfd[disk+2]) {
+                    finish(b);
+                }
+                else {
+                    diskstatus[disk+2] = 2;
+                    b->disk = disk+2;
+                    diskfd[disk+2] = b->fd;
+                    diskaccess[disk+2] = b;
+                }
+                return b_first;   
+            }
+        }
+    }
+
 }
 
 void printRAIDbuf() {
@@ -123,6 +139,17 @@ RAID_buf_ele *b = RAID_buf;
             b->next->disk = -1;
             b->next->type = type;
             b->next->ptr = ptr;
+            if(type == RAID_WRITE) {
+                b = b->next;
+                b->next = (RAID_buf_ele *) malloc(sizeof(RAID_buf_ele));
+                b->next->prev = b;
+                b->next->next = NULL;
+                b->next->fd = fd;
+                b->next->pagenum = pagenum;
+                b->next->disk = -1;
+                b->next->type = type;
+                b->next->ptr = ptr; 
+            }
             break;
         }
         b = b->next;
